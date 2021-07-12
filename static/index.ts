9140970c805278a9dfd313b4295c40ee4036e5c0
Oleg Antimonov <antimonov@ecopsy.ru>
2021-07-12 13:03:23 +0300

    Path fix

--- static/index.ts
+++ static/index.ts
@@ -1,7 +1,7 @@
-import { SignIn } from './pages/SignIn/SignIn';
-import { SignUp } from './pages/SignUp/SignUp';
-import { Home } from './pages/Home/Home';
-import { ProfileSetting } from './pages/ProfileSetting/ProfileSetting';
+import { SignIn } from './pages/SignIn/SignIn.ts';
+import { SignUp } from './pages/SignUp/SignUp.ts';
+import { Home } from './pages/Home/Home.ts';
+import { ProfileSetting } from './pages/ProfileSetting/ProfileSetting.ts';
 import { Router } from './utils/Router';
 
 export const router = new Router('.app');
