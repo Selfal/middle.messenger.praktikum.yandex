a0e1eba3fba2480e1f6d8e7a76ceebad8a6c57d7
Oleg Antimonov <antimonov@ecopsy.ru>
2021-07-12 11:59:21 +0300

    Фикс ошибки с деплоем home/index

--- static/index.ts
+++ static/index.ts
@@ -1,7 +1,7 @@
-import { SignIn } from './pages/SignIn/index';
-import { SignUp } from './pages/SignUp/index';
-import { Home } from './pages/Home/index';
-import { ProfileSetting } from './pages/ProfileSetting/index';
+import { SignIn } from './pages/SignIn/index.ts';
+import { SignUp } from './pages/SignUp/index.ts';
+import { Home } from './pages/Home/index.ts';
+import { ProfileSetting } from './pages/ProfileSetting/index.ts';
 import { Router } from './utils/Router';
 
 export const router = new Router('.app');
