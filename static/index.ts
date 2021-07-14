7a3ed6219b309656fc23af1c36d2f0fcc59f4569
Oleg Antimonov <antimonov@ecopsy.ru>
2021-07-14 11:03:04 +0300

    Fix naming

--- static/index.ts
+++ static/index.ts
@@ -1,7 +1,7 @@
 import { SignIn } from './pages/SignIn/index';
 import { SignUp } from './pages/SignUp/index';
 import { Home } from './pages/Home/index';
-import { ProfileSetting } from './pages/ProfileSetting/index';
+import { ProfileSetting } from './pages/ProfileSettings/index';
 import { Router } from './utils/Router';
 
 export const router = new Router('.app');
